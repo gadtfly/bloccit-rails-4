ae3bd6f8a295588b5460367ca344680e399b7a44
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-12-13 17:38:20 -0200

    User should be present and either be the owner of the record, or the admin user

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -1,14 +1,14 @@
 class TopicPolicy < ApplicationPolicy
 
   def create?
-    user.present? || user.role?(:admin)
+    user.present?
   end
   
   def update?
-    user.present? || record.user == user || user.role?(:admin)
+    (user.present? && record.user == user) || (user.present? && user.role?(:admin))
   end
   
   def destroy?
-    user.present? || record.user == user || user.role?(:admin)
+    (user.present? && record.user == user) || (user.present? && user.role?(:admin))
   end
 end
