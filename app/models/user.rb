0a135b7218870059f9f6b0d2c27d119a6a4f8e36
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-12-13 17:16:56 -0200

    Simplify User#role?

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader
 
-
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where( provider: auth.provider, uid: auth.uid ).first
     unless user
@@ -43,9 +42,8 @@ class User < ActiveRecord::Base
         order( 'rank DESC' ) # How to sort them
   end
 
-  ROLES = %w[member moderator admin]
-  def role?( base_role )
-    role.nil? ? false : ROLES.index( base_role.to_s ) <= ROLES.index( role )
+  def role?(base_role)
+    role == base_role.to_s
   end
 
   def favorited( post )
