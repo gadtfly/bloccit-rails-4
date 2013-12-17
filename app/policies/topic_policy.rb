a4521e12a0de27f9661a8c9e878cbfc87cd80e37
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-12-17 11:11:21 -0200

    Only admin can manage topics

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -1,2 +1,14 @@
 class TopicPolicy < ApplicationPolicy
+
+  def create?
+    user.present? && user.role?(:admin)
+  end
+  
+  def update?
+    create?
+  end
+
+  def destroy?
+    create?
+  end
 end
