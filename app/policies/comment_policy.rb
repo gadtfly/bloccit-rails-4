3f70ef1d2bf734011ba41d04406ac9e4765859b8
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2014-02-09 22:06:17 -0200

    No need for a index? rule for comments

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,9 +1,5 @@
 class CommentPolicy < ApplicationPolicy
 
-  def index?
-    true
-  end
-
   def destroy?
     user.present? && (record.user == user || user.role?(:admin) || user.role?(:moderator))
   end
