6604af60adf2ec6f189d26f779dc0db9788da33d
Mike Jewett <mikesjewett@gmail.com>
2014-03-24 14:54:09 -0400

    Updated routes for cleaner implementation

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ BlocReddit::Application.routes.draw do
   resources :topics do
     resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
-      match '/up-vote', to: 'votes#up_vote', as: :up_vote, via: :get
-      match '/down-vote', to: 'votes#down_vote', as: :down_vote, via: :get
+      get '/up-vote' => 'votes#up_vote', as: :up_vote
+      get '/down-vote' => 'votes#down_vote', as: :down_vote
       resources :favorites, only: [:create, :destroy]
     end
   end
@@ -13,7 +13,7 @@ BlocReddit::Application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :index, :update]
 
-  match 'about', to: 'welcome#about', via: :get
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
